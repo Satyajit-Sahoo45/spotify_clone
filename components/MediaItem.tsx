2ff7394baa34b43f3bc0bd6f98eac5298e500756
Satyajit <sahoosatyajit2801@gmail.com>
2023-11-22 00:41:06 +0530

    aded fetch song, middleware and render functionality

---
+++ components/MediaItem.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types";
+import Image from "next/image";
+
+interface MediaItemProps {
+  onClick?: (id: string) => void;
+  data: Song;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({ onClick, data }) => {
+  const imageUrl = useLoadImage(data);
+
+  const handleClick = () => {
+    if (onClick) {
+      return onClick(data.id);
+    }
+
+    // TODO: default turn on player
+  };
+  return (
+    <div
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+      onClick={handleClick}
+    >
+      <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+        <Image
+          className="object-cover"
+          src={imageUrl || "images/liked.png"}
+          fill
+          alt="Media Item"
+        />
+      </div>
+      <div className="flex flex-col gap-y-1 overflow-hidden">
+        <p className="text-white truncate">{data.title}</p>
+        <p className="text-neutral-400 text-sm trucate">{data.author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MediaItem;
